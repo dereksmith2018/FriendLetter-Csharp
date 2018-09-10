9bc47a69c508f945346e7adef4f821b611e3ae15
Derek Smith and Julius Bade <derek@>
2018-09-10 11:25:53 -0700

    It will submit form and change names of recepient

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -1,24 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
 
 namespace FriendLetter.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("/hello")]
-        public string Hello() { return "Hello friend!"; }
-        [Route("/goodbye")]
-        public string GoodBye() { return "GoodBye friend.";}
-
-        [Route("/")]
-        public  ActionResult Letter()
-        {
-          return View();
-        }
-        [Route("/journal")]
-        public ActionResult Journal()
-        {
-          return View();
-        }
-
+      [Route("/")]
+      public ActionResult Hello()
+      {
+        LetterVariable myLetterVariable = new LetterVariable();
+        myLetterVariable.SetRecipient("Lina");
+        myLetterVariable.SetSender("John");
+        return View(myLetterVariable);
+      }
+      [Route("/form")]
+       public ActionResult Form()
+       {
+         return View();
+       }
+      [Route("/greeting_card")]
+      public ActionResult GreetingCard()
+      {
+          LetterVariable myLetterVariable = new LetterVariable();
+          myLetterVariable.SetRecipient(Request.Query["recipient"]);
+          myLetterVariable.SetSender(Request.Query["sender"]);
+          return View("Hello", myLetterVariable);
+      }
     }
 }
